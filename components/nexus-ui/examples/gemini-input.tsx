71b803901dfae19becbee0f2265a09206c6c2152
Victor Williams <victor.williams@stu.cu.edu.ng>
2026-03-10 09:58:54 +0100

    docs: update prompt input example

--- components/nexus-ui/examples/gemini-input.tsx
+++ components/nexus-ui/examples/gemini-input.tsx
@@ -17,7 +17,7 @@ const GeminiInput = () => {
     <PromptInput className="rounded-[32px] p-3 shadow-none dark:border-none">
       <PromptInputTextarea
         placeholder="Ask Gemini 3"
-        className="min-h-12 px-3 py-2.25 text-base! placeholder:text-base"
+        className="min-h-12 px-3 py-2.25 text-base! placeholder:text-sm"
       />
       <PromptInputActions className="px-0 pt-2 pb-0">
         <PromptInputActionGroup>
@@ -27,8 +27,9 @@ const GeminiInput = () => {
             </Button>
           </PromptInputAction>
           <PromptInputAction asChild>
-            <Button className="size-10 cursor-pointer gap-1 rounded-full border-none bg-transparent text-[13px] leading-6 font-normal text-[#5D5D5D] hover:bg-gray-200 dark:text-white dark:hover:bg-gray-700">
+            <Button className="h-10 cursor-pointer gap-1.75 rounded-full border-none bg-transparent text-[13px] leading-6 font-normal text-[#5D5D5D] hover:bg-gray-200 dark:text-white dark:hover:bg-gray-700">
               <GeminiPageInfo className="size-5 text-[#5D5D5D]" />
+              <span className="max-sm:hidden">Tools</span>
             </Button>
           </PromptInputAction>
         </PromptInputActionGroup>
